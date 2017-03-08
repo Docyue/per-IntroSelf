60d800a723eacc3f3fe226b902a254eab954c59e
docyue <442497994@qq.com>
2017-03-08 09:11:48 +0800

    更新内容

---
+++ Javascript变态题解析/act-01.js
@@ -0,0 +1,6 @@
+console.log(['1','2','3'].map(parseInt))	//[ 1, NaN, NaN ]
+console.log( 0.1 + 0.2 === 0.3);	//	false
+console.log( 0.1 + 0.2 );			//	0.30000000000000004
+console.log(undefined == null);		//	true
+console.log(undefined === null);	// false
+console.log([1] == [1]);			// false
