1a84398de562ed2ace80b9dcac2eaa0b82e0aeb3
docyue <442497994@qq.com>
2017-03-28 18:42:04 +0800

    update
    Signed-off-by: docyue <442497994@qq.com>

--- promise/promise-14.js
+++ promise/promise-14.js
@@ -1,10 +1,21 @@
-let doSth = new Promise((resolve, reject) => {
-  console.log('hello');
-  resolve();
-});
+// let doSth = new Promise((resolve, reject) => {
+//   console.log('hello');
+//   resolve();
+// });
 
-setTimeout(() => {
-  doSth.then(() => {
-    console.log('over');
-  })
-}, 10000);
+// setTimeout(() => {
+//   doSth.then(() => {
+//     console.log('over');
+//   })
+// }, 3000);
+
+// let a = [1,2];
+// let b = [5,8];
+// console.log(zipWith(a,b,(a,b)=>a+b));
+
+// function zipWith(...arrays) {
+//   const length = arrays.length
+//   let iteratee = length > 1 ? arrays[length - 1] : undefined
+//   iteratee = typeof iteratee == 'function' ? (arrays.pop(), iteratee) : undefined
+//   return unzipWith(arrays, iteratee)
+// }
