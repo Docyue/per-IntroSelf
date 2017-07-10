8eba7067e2dce87bbe9579dabf6635e935216048
docyue <442497994@qq.com>
2017-07-10 18:16:02 +0800

    update

---
+++ ES6深入系列/ES6 example/es6-1.js
@@ -0,0 +1,18 @@
+function getValue(condition) {
+
+    if (condition) {
+        let value = "blue";
+
+        // other code
+
+        return value;
+    } else {
+
+        // value doesn't exist here
+
+        return null;
+    }
+
+    // value doesn't exist here
+}
+console.log(getValue(false));
