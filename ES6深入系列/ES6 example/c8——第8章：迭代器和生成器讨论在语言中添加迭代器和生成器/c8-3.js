1603e4724131e25f14b5c338f135589f95f5b603
docyue <442497994@qq.com>
2017-07-21 11:14:34 +0800

    更新  迭代器生成器

--- ES6深入系列/ES6 example/c8——第8章：迭代器和生成器讨论在语言中添加迭代器和生成器/c8-3.js
+++ ES6深入系列/ES6 example/c8——第8章：迭代器和生成器讨论在语言中添加迭代器和生成器/c8-3.js
@@ -72,6 +72,7 @@ function *createRepeatingIterator(count) {
 
 function *createCombinedIterator() {
     let result = yield *createNumberIterator();
+    yield result;
     yield *createRepeatingIterator(result);
 }
 
@@ -79,10 +80,133 @@ var iterator = createCombinedIterator();
 
 console.log(iterator.next());           // "{ value: 1, done: false }"
 console.log(iterator.next());           // "{ value: 2, done: false }"
+console.log(iterator.next());           // "{ value: 3, done: false }"
 console.log(iterator.next());           // "{ value: "repeat", done: false }"
 console.log(iterator.next());           // "{ value: "repeat", done: false }"
 console.log(iterator.next());           // "{ value: "repeat", done: false }"
 console.log(iterator.next());           // "{ value: undefined, done: true }"
+
+// ------------异步任务运行
+// JavaScript中的异步编程是一把双刃剑：简单的任务很容易做异步，而复杂的任务成为代码组织中的一个差异。
+// 由于生成器允许您在执行过程中有效地暂停代码，因此它们打开了与异步处理相关的许多可能性
+
+// 传统的执行异步操作的方法是调用回调函数,
+// 从Node.js中的磁盘读取文件
+let fs = require("fs");
+fs.readFile("config.json", function(err, contents) {
+    if (err) {
+        throw err;
+    }
+    doSomethingWith(contents);
+    console.log("Done");
+});
+
+
+// 因为yield停止执行并等待next()方法再次启动，您可以实现异步调用而不管理回调
+function run(taskDef) {
+    // create the iterator, make available elsewhere
+    let task = taskDef();
+    // start the task
+    let result = task.next();
+    // recursive function to keep calling next()
+    function step() {
+
+        // if there's more to do
+        if (!result.done) {
+            result = task.next();
+            step();
+        }
+    }
+    // start the process
+    step();
+}
+// 参数result.value传递给next()它，可以在yield调用之间传递数据
+run(function*() {
+    let value = yield 1;
+    console.log(value);         // 1
+
+    value = yield value + 3;
+    console.log(value);         // 4
+});
+
+// result.value是函数（用===运算符检查），它被调用一个回调函数。
+// 该回调函数遵循Node.js约定，它将任何可能的错误作为第一个参数（err）传递，结果作为第二个参数
+function run(taskDef) {
+
+    // create the iterator, make available elsewhere
+    let task = taskDef();
+
+    // start the task
+    let result = task.next();
+
+    // recursive function to keep calling next()
+    function step() {
+
+        // if there's more to do
+        if (!result.done) {
+            if (typeof result.value === "function") {
+                result.value(function(err, data) {
+                    if (err) {
+                        result = task.throw(err);
+                        return;
+                    }
+
+                    result = task.next(data);
+                    step();
+                });
+            } else {
+                result = task.next(result.value);
+                step();
+            }
+
+        }
+    }
+
+    // start the process
+    step();
+}
+
+
+
+
+let fs = require("fs");
+
+function readFile(filename) {
+    return function(callback) {
+        fs.readFile(filename, callback);
+    };
+}
+
+run(function*() {
+    let contents = yield readFile("config.json");
+    doSomethingWith(contents);
+    console.log("Done");
+});
+
+
+
+
+// ----------概要
+// 迭代器是ECMAScript 6的重要组成部分，是几个关键语言元素的根源。表面上，迭代器提供了一种使用简单API返回值序列的简单方法。然而，在ECMAScript 6中使用迭代器有更复杂的方法。
+
+// 该Symbol.iterator符号用于定义对象的默认迭代器。内置对象和开发人员定义的对象都可以使用此符号来提供返回迭代器的方法。当Symbol.iterator对象被提供时，该对象被认为是可迭代的。
+
+// for-of循环使用iterables 在循环中返回一系列值。使用for-of比使用传统for循环更容易，因为您不再需要跟踪值并在循环结束时进行控制。该for-of循环自动从迭代器，直到有没有更多的读取所有的值，然后退出。
+
+// 为了for-of更容易使用，ECMAScript 6中的许多值具有默认迭代器。所有的集合类型，即数组，映射和集合 - 都具有旨在使其内容易于访问的迭代器。字符串也有一个默认的迭代器，它使得迭代字符串的字符（而不是代码单元）变得容易。
+
+// 扩展运算符可以使用任何可迭代的方法，并将易于将迭代转换为数组也很容易。该转换通过从迭代器读取值并将它们单独插入到数组中起作用。
+
+// 一个生成器是一个特殊的函数，在调用时自动创建一个迭代器。发生器定义由star（*）字符表示，并且使用yield关键字来指示为每次连续调用该next()方法返回哪个值。
+
+// 生成器委托通过让新的生成器重用现有的生成器来鼓励迭代器行为的良好封装。您可以通过调用yield *而不是使用另一个生成器中的现有生成器yield。此过程允许您创建一个从多个迭代器返回值的迭代器。
+
+// 也许生成器和迭代器最有趣和令人兴奋的方面是创建更干净的异步代码的可能性。而不需要在任何地方使用回调，您可以设置看起来同步的代码，但实际上用于yield等待异步操作完成。
+
+
+
+
+
 
 
 
